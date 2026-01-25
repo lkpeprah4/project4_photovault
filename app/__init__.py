aec433522c6f6ce17ddd24c41350a4b767165cad
kwanimaa <akousakwanimaaaa@gmail.com>
2026-01-25 21:21:42 +0000

    today streak

--- app/__init__.py
+++ app/__init__.py
@@ -13,6 +13,8 @@ def create_app():
     limiter.init_app(app)
     cache.init_app(app)
 
+    cache.init_app(app)
+
   
     from .routes.auth_routes import auth_bp
     from .routes.photo_routes import photo_bp
