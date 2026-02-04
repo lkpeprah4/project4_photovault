c140375f0c7eb46c766bfee647a69100c0fb8901
kwanimaa <akousakwanimaaaa@gmail.com>
2026-02-04 22:50:38 +0000

    streak

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,3 @@
-
-
 from flask import Flask
 from .extensions import db, bcrypt, jwt, limiter, cache
  
@@ -12,25 +10,24 @@ def create_app():
     jwt.init_app(app)
     limiter.init_app(app)
     cache.init_app(app)
-    cache.init_app(app)
-
-
-  
+    
     from .routes.auth_routes import auth_bp
     from .routes.photo_routes import photo_bp
     from .routes.albums_routes import album_bp
-
+  
     app.register_blueprint(auth_bp)
     app.register_blueprint(photo_bp)
     app.register_blueprint(album_bp)
-    app.register_blueprint(album_bp)
     
-    #     # DEBUG: print all registered routes
+    return app
+    
+    # DEBUG: print all registered routes
     # print("\n[DEBUG] Registered routes:")
-    app.register_blueprint(album_bp)
+    
     # for rule in app.url_map.iter_rules():
     #     print(f"{rule} -> {rule.endpoint}")
     # print("[END DEBUG]\n")
+    
 
 
-    return app
+    
